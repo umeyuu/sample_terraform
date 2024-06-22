1396b16e44daf8481b999ffc8f1515a5785a53e8
umeyuu <91179464+umeyuu@users.noreply.github.com>
2024-06-22 22:34:47 +0900

    deploy server

--- main.tf
+++ main.tf
@@ -2,11 +2,46 @@ provider "aws" {
     region = "us-east-2"
 }
 
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests."
+    type        = number
+    default     = 8080
+}
+
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP of the web server."
+}
+
+
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+    
+    user_data_replace_on_change = true
 
     tags = {
         Name = "teraform-example"
+    }
+}
+
+
+resource "aws_security_group" "instance" {
+    name = "teraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
