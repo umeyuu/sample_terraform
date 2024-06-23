b96023c465ca349ca01143060073e787211ff1a2
umeyuu <91179464+umeyuu@users.noreply.github.com>
2024-06-23 13:13:25 +0900

    deploy cluster and load balancer

--- main.tf
+++ main.tf
@@ -1,47 +1,157 @@
+
+
+
 provider "aws" {
-    region = "us-east-2"
-}
-
-
-variable "server_port" {
-    description = "The port the server will use for HTTP requests."
-    type        = number
-    default     = 8080
-}
-
-
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP of the web server."
-}
-
-
-resource "aws_instance" "example" {
-    ami = "ami-0fb653ca2d3203ac1"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
-
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello World" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
-    
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "teraform-example"
-    }
+  region = "us-east-2"
 }
 
 
 resource "aws_security_group" "instance" {
-    name = "teraform-example-instance"
+  name = "terraform-example-instance"
 
-    ingress {
-        from_port = var.server_port
-        to_port = var.server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  # Autoscaling Groupがある起動設定を使った場合に必須
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  # 起動設定（現在はもう使わず、起動テンプレートを使う）
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  # ASGとALBのインテグレーション
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB" # ALBのヘルスチェックを行い、正常でない場合に自動的に置き換える指示をASGに出す
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# データソース
+# <PROVIDER>_<TYPE>
+data "aws_vpc" "default" {
+  default = true  # デフォルトのVPCを取得
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  # subnets = data.aws_subnet_ids.default.ids # 誤植？
+  subnets = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  # デフォルトではシンプルな404エラーページを返す
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page Not Found"
+      status_code = 404
     }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # インバウンドHTTPリクエストを許可
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # アウトバウンドリクエストを全て許可
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# リクエストを、ASGが含まれるターゲットグループに送る、リスナールール
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
